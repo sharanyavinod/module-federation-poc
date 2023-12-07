e80417d30ad19bab10bf8acf2647edc8d0cedc94
svinod <svinod@adobe.com>
2023-12-07 08:48:10 +0100

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+
+export default {
+  input: "src/index.jsx",
+  output: {
+    file: "dist/test.js",
+    format: "es",
+    name: "RemoteApp",
+    // sourcemap: "inline",
+    // inlineDynamicImports: true,
+    // globals: {
+    //   "react/jsx-runtime": "jsxRuntime",
+    //   "react-dom/client": "ReactDOM",
+    //   react: "React",
+    // },
+  },
+  plugins: [
+    resolve(),
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      presets: [["@babel/preset-react", { runtime: "automatic" }]],
+      plugins: ["@babel/plugin-syntax-dynamic-import"],
+    }),
+    // replace({
+    //   'Object.defineProperty(exports, "__esModule", { value: true });': "",
+    //   delimiters: ["\n", "\n"],
+    // }),
+  ],
+};
---
+++ src/index.jsx
@@ -0,0 +1,3 @@
+export const RemoteApp = () => {
+  return <div>Hello from a remote component!!</div>;
+};
